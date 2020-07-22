03dc87aeaf032fe2d911555af6fd1625588e92d7
Admin <liumingli95@gmail.com>
2020-07-23 01:14:26 +0800

    truism-cms-web

---
+++ src/views/staff/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <el-button>
+
+        </el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: [],
+            total: 0,
+            currentPage: 1,
+            pageSize: 10,
+
+        }
+
+    },
+
+    created() {
+
+    },
+
+    methods: {
+
+    },
+}
+</script>
+
+<style>
+
+</style>
