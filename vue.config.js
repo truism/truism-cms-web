ed40e58e46f22fde2028f4d401e9c91a0e9c8e19
liliuming <liumingli95@gmail.com>
2020-08-01 03:18:58 +0800

    修改默认端口，避免和前台冲突

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     devServer: {
-        port: 8888, // 端口号，如果端口号被占用，会自动提升1
+        port: 9999, // 端口号，如果端口号被占用，会自动提升1
         host: "localhost", //主机名， 127.0.0.1，  真机 0.0.0.0
         https: false, //协议
         open: true, //启动服务时自动打开浏览器访问
-    
+
         proxy: {
             [process.env.VUE_APP_BASE_API]: {
                 target: process.env.VUE_APP_SERVICE_URL,
@@ -17,5 +17,5 @@ module.exports = {
     },
 
     lintOnSave: false, // 关闭格式检查
-    productionSourceMap: false, // 打包时不会生成 .map 文件，加快打包速度 
+    productionSourceMap: false, // 打包时不会生成 .map 文件，加快打包速度
 }
